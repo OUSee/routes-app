b97b905ea8bf1d6136960bc92e971ed50ef8def1
OUSee <newerworrying@gmail.com>
2024-07-22 13:22:16 +0300

    build created and racoon added

--- src/components/Crew/Crew.tsx
+++ src/components/Crew/Crew.tsx
@@ -3,8 +3,8 @@ import styles from "./styles.module.css"
 export const Crew = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.card}>
-                <div className={styles.face}></div>
+            <div className={styles.card}>               
+                <img className={styles.racoon} src="https://media.tenor.com/tCL3HGcaV4UAAAAM/raccoon-dance.gif" />
                 <h4>John Doe</h4>
                 <a href="https://en.wikipedia.org/wiki/John_Doe">@linktosocial</a>
             </div>
